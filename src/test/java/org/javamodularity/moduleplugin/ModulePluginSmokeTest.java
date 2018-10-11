3b2c4787770a42ce737955a438dd3347fba39d34
Mykyta Protsenko <mprotsenko@netflix.com>
2018-10-11 17:37:18 +0000

    Merge pull request #3 in EDX/gradle-module-plugin from fix-apply-plugin to master
    Squashed commit of the following:
    
    commit 566f5fa17f6d548f8bcc2504c6444675da0322d5
    Author: Mykyta Protsenko <mprotsenko@netflix.com>
    Date:   Thu Oct 11 10:35:18 2018 -0700
    
        Added gradle magic to provide the plugin classpath to subprojects, moved plugin application from 'subprojects' block to subprojects' build.gradle files
    
        Fixed the test to verify the build status of subprojects

--- src/test/java/org/javamodularity/moduleplugin/ModulePluginSmokeTest.java
+++ src/test/java/org/javamodularity/moduleplugin/ModulePluginSmokeTest.java
@@ -1,26 +1,50 @@
 package org.javamodularity.moduleplugin;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import org.gradle.testkit.runner.GradleRunner;
 import org.gradle.testkit.runner.TaskOutcome;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SuppressWarnings("ConstantConditions")
 class ModulePluginSmokeTest {
+
+    private List<File> pluginClasspath;
+
+    @BeforeEach
+    void before() throws IOException {
+        pluginClasspath = Resources.readLines(Resources.getResource("plugin-classpath.txt"), Charsets.UTF_8)
+                .stream()
+                .map(fname -> new File(fname))
+                .collect(Collectors.toList());
+    }
 
     @Test
     void smokeTest() {
         var result = GradleRunner.create()
                 .withProjectDir(new File("test-project"))
-                .withPluginClasspath()
+                .withPluginClasspath(pluginClasspath)
                 .withGradleVersion("4.10.2")
                 .withArguments("clean", "build")
                 .build();
 
-        assertEquals(TaskOutcome.SUCCESS, result.task(":build").getOutcome(),
-                "Failed Build: "+ result.getOutput());
+
+        System.out.println("Build result");
+        System.out.println("============");
+        System.out.println(result.getOutput());
+        System.out.println("============");
+        assertEquals(TaskOutcome.SUCCESS, result.task(":greeter.api:build").getOutcome(), "Failed Build!");
+        assertEquals(TaskOutcome.SUCCESS, result.task(":greeter.provider:build").getOutcome(), "Failed Build!");
+        assertEquals(TaskOutcome.SUCCESS, result.task(":greeter.provider.test:build").getOutcome(), "Failed Build!");
+        assertEquals(TaskOutcome.SUCCESS, result.task(":greeter.runner:build").getOutcome(), "Failed Build!");
     }
 }
