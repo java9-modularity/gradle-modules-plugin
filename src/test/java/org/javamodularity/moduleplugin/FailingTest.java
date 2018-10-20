a8f0e1f8710e4d0a9608eff601f86eac4da7ea89
Paul Bakker <pbakker@netflix.com>
2018-10-20 15:16:24 -0700

    Testing travis

---
+++ src/test/java/org/javamodularity/moduleplugin/FailingTest.java
@@ -0,0 +1,12 @@
+package org.javamodularity.moduleplugin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FailingTest {
+    @Test
+    public void mustFail() {
+        fail("This is supposed to fail, testing Travis builds");
+    }
+}
