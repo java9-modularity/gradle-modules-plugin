e5d871801d86885ad1ad822f91f23c24e0f48fd0
Ihor Herasymenko <exrolexx@gmail.com>
2018-10-20 19:53:15 +0300

    Polishing
    * De-duplicate Test engine resolution code
    * Code cleanup

--- src/main/java/org/javamodularity/moduleplugin/TestEngine.java
+++ src/main/java/org/javamodularity/moduleplugin/TestEngine.java
@@ -1,48 +1,43 @@
 package org.javamodularity.moduleplugin;
 
+import org.gradle.api.Project;
+
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public enum TestEngine {
-    JUNIT_4("junit", "junit", "junit", "JUnit 4", "junit"),
-    JUNIT_5("org.junit.jupiter", "junit-jupiter-api", "org.junit.jupiter.api", "JUnit 5", "org.junit.platform.commons"),
-    TESTNG("org.testng", "testng", "testng", "TestNG", "testng");
+
+    JUNIT_4("junit", "junit", "junit", "junit"),
+    JUNIT_5("org.junit.jupiter", "junit-jupiter-api", "org.junit.jupiter.api", "org.junit.platform.commons"),
+    TESTNG("org.testng", "testng", "testng", "testng");
 
     private final String groupId;
     private final String artifactId;
-    private final String moduleName;
-    private final String name;
-    private final String addOpens;
 
-    TestEngine(String groupId, String artifactId, String moduleName, String name, String addOpens) {
+    public final String moduleName;
+    public final String addOpens;
+
+    TestEngine(String groupId, String artifactId, String moduleName, String addOpens) {
         this.groupId = groupId;
         this.artifactId = artifactId;
         this.moduleName = moduleName;
-        this.name = name;
         this.addOpens = addOpens;
     }
 
-    public String getGroupId() {
-        return groupId;
+    public static Optional<TestEngine> select(Project project) {
+        var configurations = project.getConfigurations();
+        var testImplementation = configurations.getByName("testImplementation").getDependencies().stream();
+        var testCompile = configurations.getByName("testCompile").getDependencies().stream();
+        return Stream.concat(testImplementation, testCompile)
+                .map(d -> TestEngine.select(d.getGroup(), d.getName()))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findAny();
     }
 
-    public String getArtifactId() {
-        return artifactId;
-    }
-
-    public String getModuleName() {
-        return moduleName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getAddOpens() {
-        return addOpens;
-    }
-
-    public static Optional<TestEngine> select(String groupId, String artifactId) {
+    private static Optional<TestEngine> select(String groupId, String artifactId) {
         return Arrays.stream(TestEngine.values()).filter(engine -> engine.groupId.equals(groupId) && engine.artifactId.equals(artifactId)).findAny();
     }
+
 }
--- src/main/java/org/javamodularity/moduleplugin/tasks/CompileTestTask.java
+++ src/main/java/org/javamodularity/moduleplugin/tasks/CompileTestTask.java
@@ -9,14 +9,12 @@ import org.javamodularity.moduleplugin.TestEngine;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 public class CompileTestTask {
+
     public void configureCompileTestJava(Project project, String moduleName) {
-        final JavaCompile compileTestJava = (JavaCompile) project.getTasks().findByName(JavaPlugin.COMPILE_TEST_JAVA_TASK_NAME);
-        JavaPluginConvention javaConvention =
-                project.getConvention().getPlugin(JavaPluginConvention.class);
+        JavaCompile compileTestJava = (JavaCompile) project.getTasks().findByName(JavaPlugin.COMPILE_TEST_JAVA_TASK_NAME);
+        JavaPluginConvention javaConvention = project.getConvention().getPlugin(JavaPluginConvention.class);
 
         SourceSet testSourceSet = javaConvention.getSourceSets().getByName(SourceSet.TEST_SOURCE_SET_NAME);
 
@@ -27,24 +25,15 @@ public class CompileTestTask {
                     "--patch-module", moduleName + "=" + testSourceSet.getJava().getSourceDirectories().getAsPath()
             ));
 
-            var configurations = project.getConfigurations();
-            var testImplementation = configurations.getByName("testImplementation").getDependencies().stream();
-            var testCompile = configurations.getByName("testCompile").getDependencies().stream();
-            Optional<TestEngine> testEngine = Stream.concat(testImplementation, testCompile)
-                    .map(d -> TestEngine.select(d.getGroup(), d.getName()))
-                    .filter(Optional::isPresent)
-                    .map(Optional::get)
-                    .findAny();
-
-            if (testEngine.isPresent()) {
-                String testEngineModuleName = testEngine.get().getModuleName();
+            TestEngine.select(project).ifPresent(testEngine -> {
                 args.addAll(List.of(
-                        "--add-modules", testEngineModuleName,
-                        "--add-reads", moduleName + "=" + testEngineModuleName));
-            }
+                        "--add-modules", testEngine.moduleName,
+                        "--add-reads", moduleName + "=" + testEngine.moduleName));
+            });
 
             compileTestJava.getOptions().setCompilerArgs(args);
             compileTestJava.setClasspath(project.files());
         });
     }
+
 }
