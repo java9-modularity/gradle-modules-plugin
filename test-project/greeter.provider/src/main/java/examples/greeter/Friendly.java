1447dec3c913bd6cade3dbcd4019d021c9047d61
Paul Bakker <pbakker@netflix.com>
2018-11-01 21:36:19 -0700

    --patch-module so that resources are loaded correctly in tests

--- test-project/greeter.provider/src/main/java/examples/greeter/Friendly.java
+++ test-project/greeter.provider/src/main/java/examples/greeter/Friendly.java
@@ -1,10 +1,17 @@
 package examples.greeter;
 
 import examples.greeter.api.Greeter;
+import java.io.*;
+import java.util.*;
 
 public class Friendly implements Greeter {
     @Override
     public String hello() {
-        return "Hello and welcome!";
+        var stream = this.getClass().getResourceAsStream("/greeting.txt");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "utf-8"))) {
+            return reader.readLine();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
