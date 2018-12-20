37ea408f92aa0195edb4de3004f4d3d5b27f4f27
Serban Iordache <iordache@hotmail.com>
2018-12-20 21:37:45 +0100

    issue #53: apply module-info.test only to test-related tasks (#56)
    * issue #53: apply command line options from module-info.test only to test-related tasks
    
    * use hamcrest 2.1+

--- test-project/greeter.provider/src/test/java/examples/greeter/ScriptingTest.java
+++ test-project/greeter.provider/src/test/java/examples/greeter/ScriptingTest.java
@@ -2,12 +2,14 @@ package examples.greeter;
 
 import javax.script.*;
 import org.junit.jupiter.api.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 class ScriptingTest {
 
     @Test
     void testScripting() {
         ScriptEngineManager manager = new ScriptEngineManager();
-        Assertions.assertNotNull(manager.getEngineFactories());
+        assertThat(manager.getEngineFactories(), not(nullValue()));
     }
 }
