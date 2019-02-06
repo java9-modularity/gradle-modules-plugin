1ebe0163a9d4cf6e1121a2e61e220e729bd24cb2
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2019-02-06 01:38:07 +0000

    #54: Replace Lambda with anonymous inner classes, continued (#64)

--- src/main/java/org/javamodularity/moduleplugin/tasks/RunTaskMutator.java
+++ src/main/java/org/javamodularity/moduleplugin/tasks/RunTaskMutator.java
@@ -3,6 +3,7 @@ package org.javamodularity.moduleplugin.tasks;
 import org.gradle.api.Action;
 import org.gradle.api.GradleException;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.distribution.Distribution;
 import org.gradle.api.distribution.DistributionContainer;
 import org.gradle.api.file.CopySpec;
@@ -53,44 +54,50 @@ public class RunTaskMutator {
     public void updateStartScriptsTask(CreateStartScripts startScriptsTask) {
         PatchModuleExtension patchModuleExtension = project.getExtensions().getByType(PatchModuleExtension.class);
 
-        startScriptsTask.doFirst(task -> {
-            startScriptsTask.setClasspath(project.files());
-            if (ModularCreateStartScripts.UNDEFINED_MAIN_CLASS_NAME.equals(startScriptsTask.getMainClassName())) {
-                startScriptsTask.setMainClassName(/* moduleName + "/" + */ execTask.getMain());
+        startScriptsTask.doFirst(new Action<Task>() {
+            @Override
+            public void execute(final Task task) {
+                startScriptsTask.setClasspath(project.files());
+                if (ModularCreateStartScripts.UNDEFINED_MAIN_CLASS_NAME.equals(startScriptsTask.getMainClassName())) {
+                    startScriptsTask.setMainClassName(/* moduleName + "/" + */ execTask.getMain());
+                }
+
+                var moduleJvmArgs = List.of(
+                        "--module-path", LIBS_PLACEHOLDER,
+                        "--module", RunTaskMutator.this.getMainClass()
+                );
+
+                var jvmArgs = new ArrayList<String>();
+
+                ModuleOptions moduleOptions = execTask.getExtensions().getByType(ModuleOptions.class);
+                if (!moduleOptions.getAddModules().isEmpty()) {
+                    String addModules = String.join(",", moduleOptions.getAddModules());
+                    jvmArgs.add("--add-modules");
+                    jvmArgs.add(addModules);
+                }
+
+                patchModuleExtension.getConfig().forEach(patch -> {
+                            String[] split = patch.split("=");
+                            jvmArgs.add("--patch-module");
+                            jvmArgs.add(split[0] + "=" + PATCH_LIBS_PLACEHOLDER + "/" + split[1]);
+                        }
+                );
+
+                startScriptsTask.getDefaultJvmOpts().forEach(jvmArgs::add);
+                jvmArgs.addAll(moduleJvmArgs);
+
+                startScriptsTask.setDefaultJvmOpts(jvmArgs);
             }
-
-            var moduleJvmArgs = List.of(
-                    "--module-path", LIBS_PLACEHOLDER,
-                    "--module", getMainClass()
-            );
-
-            var jvmArgs = new ArrayList<String>();
-
-            ModuleOptions moduleOptions = execTask.getExtensions().getByType(ModuleOptions.class);
-            if (!moduleOptions.getAddModules().isEmpty()) {
-                String addModules = String.join(",", moduleOptions.getAddModules());
-                jvmArgs.add("--add-modules");
-                jvmArgs.add(addModules);
-            }
-
-            patchModuleExtension.getConfig().forEach(patch -> {
-                        String[] split = patch.split("=");
-                        jvmArgs.add("--patch-module");
-                        jvmArgs.add(split[0] + "=" + PATCH_LIBS_PLACEHOLDER + "/" + split[1]);
-                    }
-            );
-
-            startScriptsTask.getDefaultJvmOpts().forEach(jvmArgs::add);
-            jvmArgs.addAll(moduleJvmArgs);
-
-            startScriptsTask.setDefaultJvmOpts(jvmArgs);
         });
 
-        startScriptsTask.doLast(task -> {
-            File bashScript = new File(startScriptsTask.getOutputDir(), startScriptsTask.getApplicationName());
-            replaceLibsPlaceHolder(bashScript.toPath(), "\\$APP_HOME/lib", "\\$APP_HOME/patchlibs");
-            File batFile = new File(startScriptsTask.getOutputDir(), startScriptsTask.getApplicationName() + ".bat");
-            replaceLibsPlaceHolder(batFile.toPath(), "%APP_HOME%\\\\lib", "%APP_HOME%\\\\patchlibs");
+        startScriptsTask.doLast(new Action<Task>() {
+            @Override
+            public void execute(final Task task) {
+                File bashScript = new File(startScriptsTask.getOutputDir(), startScriptsTask.getApplicationName());
+                RunTaskMutator.this.replaceLibsPlaceHolder(bashScript.toPath(), "\\$APP_HOME/lib", "\\$APP_HOME/patchlibs");
+                File batFile = new File(startScriptsTask.getOutputDir(), startScriptsTask.getApplicationName() + ".bat");
+                RunTaskMutator.this.replaceLibsPlaceHolder(batFile.toPath(), "%APP_HOME%\\\\lib", "%APP_HOME%\\\\patchlibs");
+            }
         });
     }
 
@@ -111,38 +118,41 @@ public class RunTaskMutator {
     }
 
     private void updateJavaExecTask() {
-        execTask.doFirst(task -> {
+        execTask.doFirst(new Action<Task>() {
+            @Override
+            public void execute(final Task task) {
 
-            JavaPluginConvention javaConvention = execTask.getProject().getConvention().getPlugin(JavaPluginConvention.class);
-            PatchModuleExtension patchModuleExtension = project.getExtensions().getByType(PatchModuleExtension.class);
+                JavaPluginConvention javaConvention = execTask.getProject().getConvention().getPlugin(JavaPluginConvention.class);
+                PatchModuleExtension patchModuleExtension = project.getExtensions().getByType(PatchModuleExtension.class);
 
-            SourceSet mainSourceSet = javaConvention.getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME);
+                SourceSet mainSourceSet = javaConvention.getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME);
 
-            var moduleJvmArgs = List.of(
-                    "--module-path", execTask.getClasspath()
-                            .filter(patchModuleExtension::isUnpatched
-                            ).getAsPath(),
-                    "--patch-module", moduleName + "=" + mainSourceSet.getOutput().getResourcesDir().toPath(),
-                    "--module", getMainClass()
-            );
+                var moduleJvmArgs = List.of(
+                        "--module-path", execTask.getClasspath()
+                                .filter(patchModuleExtension::isUnpatched
+                                ).getAsPath(),
+                        "--patch-module", moduleName + "=" + mainSourceSet.getOutput().getResourcesDir().toPath(),
+                        "--module", RunTaskMutator.this.getMainClass()
+                );
 
-            var jvmArgs = new ArrayList<String>();
+                var jvmArgs = new ArrayList<String>();
 
-            ModuleOptions moduleOptions = execTask.getExtensions().getByType(ModuleOptions.class);
-            if (!moduleOptions.getAddModules().isEmpty()) {
-                String addModules = String.join(",", moduleOptions.getAddModules());
-                jvmArgs.add("--add-modules");
-                jvmArgs.add(addModules);
+                ModuleOptions moduleOptions = execTask.getExtensions().getByType(ModuleOptions.class);
+                if (!moduleOptions.getAddModules().isEmpty()) {
+                    String addModules = String.join(",", moduleOptions.getAddModules());
+                    jvmArgs.add("--add-modules");
+                    jvmArgs.add(addModules);
+                }
+
+                jvmArgs.addAll(patchModuleExtension.configure(execTask.getClasspath()));
+
+                jvmArgs.addAll(execTask.getJvmArgs());
+                jvmArgs.addAll(moduleJvmArgs);
+
+                execTask.setClasspath(project.files());
+
+                execTask.setJvmArgs(jvmArgs);
             }
-
-            jvmArgs.addAll(patchModuleExtension.configure(execTask.getClasspath()));
-
-            jvmArgs.addAll(execTask.getJvmArgs());
-            jvmArgs.addAll(moduleJvmArgs);
-
-            execTask.setClasspath(project.files());
-
-            execTask.setJvmArgs(jvmArgs);
         });
     }
 
