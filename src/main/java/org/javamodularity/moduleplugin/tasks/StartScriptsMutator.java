e7b6ee7e5bd2163371ce97925a9dbad80982a506
Serban Iordache <iordache@hotmail.com>
2021-09-15 16:12:08 +0200

    issue #206: Deprecation warning for Gradle 8.0 in startScripts action

--- src/main/java/org/javamodularity/moduleplugin/tasks/StartScriptsMutator.java
+++ src/main/java/org/javamodularity/moduleplugin/tasks/StartScriptsMutator.java
@@ -72,10 +72,6 @@ public class StartScriptsMutator extends AbstractExecutionMutator {
             if (ModularCreateStartScripts.UNDEFINED_MAIN_CLASS_NAME.equals(startScriptsTask.getMainClassName())) {
                 startScriptsTask.setMainClassName(execTask.getMain());
             }
-        } else {
-            if (ModularCreateStartScripts.UNDEFINED_MAIN_CLASS_NAME.equals(startScriptsTask.getMainClass().getOrElse(ModularCreateStartScripts.UNDEFINED_MAIN_CLASS_NAME))) {
-                startScriptsTask.getMainClass().set(execTask.getMainClass().getOrElse(ModularCreateStartScripts.UNDEFINED_MAIN_CLASS_NAME));
-            }
         }
     }
 
