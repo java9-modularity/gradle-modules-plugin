cd90e160e8c92c7d09e662a14e06272d4904970a
Kengo TODA <skypencil@gmail.com>
2022-03-02 11:55:39 +0800

    chore: remove needless imports

--- src/main/java/org/javamodularity/moduleplugin/extensions/DefaultModularityExtension.java
+++ src/main/java/org/javamodularity/moduleplugin/extensions/DefaultModularityExtension.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.gradle.api.JavaVersion;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
-import org.gradle.api.invocation.Gradle;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.compile.JavaCompile;
