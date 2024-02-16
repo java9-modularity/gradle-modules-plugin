6bea5c220b296c7efdcae4ea37377995fea268b7
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2024-02-16 14:02:17 +0000

    Set next dev version to 1.8.13-SNAPSHOT

--- test-project-kotlin/build.gradle.kts
+++ test-project-kotlin/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10" apply false
-    id("org.javamodularity.moduleplugin") version "1.8.12" apply false
+    id("org.javamodularity.moduleplugin") version "1.8.13" apply false
 }
 
 subprojects {
