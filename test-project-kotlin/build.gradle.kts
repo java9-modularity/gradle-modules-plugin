360875ef35725bb887e031741202e93630d44178
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2026-09-10 15:01:31 +0100

    Release prep: 2.1.0 (#377)
    * Release prep: 2.1.0
    
    
    
    ---------
    
    Co-authored-by: Andy Coates <big-andy-coates@users.noreply.github.com>

--- test-project-kotlin/build.gradle.kts
+++ test-project-kotlin/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "2.4.10" apply false
-    id("org.javamodularity.moduleplugin") version "2.0.1" apply false
+    id("org.javamodularity.moduleplugin") version "2.1.0" apply false
 }
 
 subprojects {
